fde8dafcf13e5e81de9b2a79c3f260f634a8ad19
eskildt <eskildtangen@gmail.com>
2019-10-09 12:25:09 +0200

    Initial commit
    *ProductDao
    *WebshopTest

---
+++ src/main/java/no/kristiania/jdbc/ProductDao.java
@@ -0,0 +1,17 @@
+package no.kristiania.jdbc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductDao {
+
+    private List<String> products = new ArrayList<>();
+
+    public void insertProduct(String productName){
+        products.add(productName);
+    }
+
+    public List<String> listAll(){
+        return products;
+    }
+}
---
+++ src/test/java/no/kristiania/jdbc/WebshopTest.java
@@ -0,0 +1,23 @@
+package no.kristiania.jdbc;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class WebshopTest {
+
+    @Test
+    void shouldRetrieveStoredProduct(){
+        ProductDao dao = new ProductDao();
+        String productName = pickOne(new String[]{"Apples", "Bananas", "Coconuts", "Dates"});
+        dao.insertProduct(productName);
+        assertThat(dao.listAll()).contains(productName);
+    }
+
+    private String pickOne(String[] strings){
+        return strings[new Random().nextInt(strings.length)];
+    }
+
+}
