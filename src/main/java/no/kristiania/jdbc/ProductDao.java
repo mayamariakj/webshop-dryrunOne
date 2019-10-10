3e2ca6e9599fc03d3934dbdbcad68f721d1ccdf4
eskildt <eskildtangen@gmail.com>
2019-10-10 16:48:24 +0200

    Add PostgreSQL support

--- src/main/java/no/kristiania/jdbc/ProductDao.java
+++ src/main/java/no/kristiania/jdbc/ProductDao.java
@@ -1,12 +1,18 @@
 package no.kristiania.jdbc;
 
+import org.postgresql.ds.PGSimpleDataSource;
+
 import javax.sql.DataSource;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
+import java.util.Scanner;
 
 public class ProductDao {
 
@@ -45,5 +51,22 @@ public class ProductDao {
                 }
             }
         }
+    }
+
+    public static void main(String[] args) throws SQLException, IOException {
+        System.out.println("Enter a product name to insert: ");
+        String productName = new Scanner(System.in).nextLine();
+
+        Properties properties = new Properties();
+        properties.load(new FileReader("webshop.properties"));
+
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/webshop");
+        dataSource.setUser("webshop");
+        dataSource.setPassword(properties.getProperty("dataSource.password"));
+        ProductDao productDao = new ProductDao(dataSource);
+        productDao.insertProduct(productName);
+
+        System.out.println(productDao.listAll());
     }
 }
